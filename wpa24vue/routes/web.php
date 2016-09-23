28bc008d3459ec887180258a9d56e94d66b8fc50
htetoozin <htetoo.zi09@gmail.com>
2016-09-23 17:44:27 +0630

    Add Vue

---
+++ wpa24vue/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function() {
+    return view('welcome');
+});
+
+
+//Route::get("main", 'MainController@index'); //For Index.blade.php
+//Route::get("vue", "VueController@vue"); //For index-vue.blade.php
+Route::get("data", 'AjaxController@data');
